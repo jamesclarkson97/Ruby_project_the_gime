1df0f58dcc4038ec10906a4465ff4234ed28c195
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-07-13 14:03:45 +0100

    created sub controllers

---
+++ controllers/enrollments_controller.rb
@@ -0,0 +1,6 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('../models/enrollment.rb')
+require_relative('../models/member.rb')
+require_relative('../models/gym_class.rb')
+also_reload('../models/*')
