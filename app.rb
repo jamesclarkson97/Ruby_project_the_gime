1f8862ae02b6372cab9de9b4c234c257477075d4
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-07-13 14:34:41 +0100

    got print on /classes

--- app.rb
+++ app.rb
@@ -3,7 +3,14 @@ require('sinatra/contrib/all')
 require_relative('controllers/members_controller')
 require_relative('controllers/gym_classes_controller')
 require_relative('controllers/enrollments_controller')
+also_reload('models/*')
 
 get '/' do
     erb(:index)
+end
+
+get '/classes' do
+    "Hello World"
+    # @gym_classes = GymClasses.all()
+    # erb(:"gym_classes/index")
 end
--- controllers/gym_classes_controller.rb
+++ controllers/gym_classes_controller.rb
@@ -2,3 +2,9 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/gym_class.rb')
 also_reload('../models/*')
+
+get '/classes' do
+    "Hello World"
+    # @gym_classes = GymClasses.all()
+    # erb(:"gym_classes/index")
+end
