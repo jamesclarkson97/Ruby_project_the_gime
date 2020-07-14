d669b54b74adf2980c1b7c36b9a76a8643ef2140
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-07-14 10:55:57 +0100

    added classes show

--- controllers/gym_classes_controller.rb
+++ controllers/gym_classes_controller.rb
@@ -4,7 +4,11 @@ require_relative('../models/gym_class.rb')
 also_reload('../models/*')
 
 get '/classes' do
-    # "Hello World"
     @gym_classes = GymClass.all()
     erb(:"gym_classes/index")
+end
+
+get '/classes/:id' do
+    @gym_class = GymClass.find(params['id'].to_i)
+    erb(:"gym_classes/show")
 end
