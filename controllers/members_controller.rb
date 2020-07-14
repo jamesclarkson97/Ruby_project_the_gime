8c5e3e3fcf8cbbac2648ffa6036d98e5be1a22bb
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-07-14 11:40:45 +0100

    added edit member info

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -23,3 +23,15 @@ post '/members' do
     @members = Member.all()
     erb(:"members/index")
 end
+
+get '/members/:id/edit' do
+    @member = Member.find(params['id'].to_i)
+    erb(:"members/edit")
+end
+
+post '/members/:id' do
+    @member = Member.new(params)
+    @member.update
+    @members = Member.all()
+    erb(:"members/index")
+end
