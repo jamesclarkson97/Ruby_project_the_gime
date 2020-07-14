144b0656f4c2d8aaaebb0e419ed8ef6513195ba1
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-07-14 13:47:58 +0100

    delete class success, but listed as a get

--- controllers/gym_classes_controller.rb
+++ controllers/gym_classes_controller.rb
@@ -3,20 +3,24 @@ require('sinatra/contrib/all')
 require_relative('../models/gym_class.rb')
 also_reload('../models/*')
 
+# INDEX
 get '/classes' do
     @gym_classes = GymClass.all()
     erb(:"gym_classes/index")
 end
 
+# NEW
 get '/classes/new' do
     erb(:"gym_classes/new")
 end
 
+# SHOW
 get '/classes/:id' do
     @gym_class = GymClass.find(params['id'].to_i)
     erb(:"gym_classes/show")
 end
 
+# CREATE
 post '/classes' do
     @gym_class = GymClass.new(params)
     @gym_class.save
@@ -24,11 +28,19 @@ post '/classes' do
     erb(:"gym_classes/index")
 end
 
+# EDIT
 get '/classes/:id/edit' do
     @gym_class = GymClass.find(params['id'].to_i)
     erb(:"gym_classes/edit")
 end
 
+# DESTROY
+get '/classes/:id/delete' do
+    GymClass.delete(params['id'].to_i)
+    redirect to("/classes")
+end
+
+# UPDATE
 post '/classes/:id' do
     @gym_class = GymClass.new(params)
     @gym_class.update
