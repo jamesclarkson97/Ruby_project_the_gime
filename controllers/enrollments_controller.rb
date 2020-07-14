f0e87c4c1719b744ee7d9770c0fc94dc2af91d55
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-07-14 14:42:36 +0100

    enroll member in class didn't crash, but updated class instead

--- controllers/enrollments_controller.rb
+++ controllers/enrollments_controller.rb
@@ -7,5 +7,15 @@ also_reload('../models/*')
 
 # NEW
 get '/classes/:id/enroll' do
+    @gym_class = GymClass.find(params['id'].to_i)
+    @members = Member.all()
     erb(:"enrollments/new")
 end
+
+# CREATE
+# post '/classes/:id' do
+#     @enrollment = Enrollment.new(params)
+#     @enrollment.save
+#     @gym_classes = GymClass.all()
+#     erb(:"gym_classes/index")
+# end
