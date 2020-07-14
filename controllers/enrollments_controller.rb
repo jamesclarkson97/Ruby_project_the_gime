d50b0f31307aca34bc8aab8adb19c4debd39325b
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-07-14 14:12:12 +0100

    added enroll member button in class show

--- controllers/enrollments_controller.rb
+++ controllers/enrollments_controller.rb
@@ -4,3 +4,8 @@ require_relative('../models/enrollment.rb')
 require_relative('../models/member.rb')
 require_relative('../models/gym_class.rb')
 also_reload('../models/*')
+
+# NEW
+get '/classes/:id/enroll' do
+    erb(:"enrollments/new")
+end
