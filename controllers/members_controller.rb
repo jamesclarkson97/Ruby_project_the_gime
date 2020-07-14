1508756bf1e32bd056c34106b20301a9009d95a0
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-07-14 13:40:45 +0100

    added delete member to page

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -3,20 +3,24 @@ require('sinatra/contrib/all')
 require_relative('../models/member.rb')
 also_reload('../models/*')
 
+# INDEX
 get '/members' do
     @members = Member.all()
     erb(:"members/index")
 end
 
+# NEW
 get '/members/new' do
     erb(:"members/new")
 end
 
+# SHOW
 get '/members/:id' do
     @member = Member.find(params['id'].to_i)
     erb(:"members/show")
 end
 
+# CREATE
 post '/members' do
     @member = Member.new(params)
     @member.save
@@ -24,11 +28,19 @@ post '/members' do
     erb(:"members/index")
 end
 
+# EDIT
 get '/members/:id/edit' do
     @member = Member.find(params['id'].to_i)
     erb(:"members/edit")
 end
 
+# DESTROY
+post '/members/:id/delete' do
+    Member.delete(params['id'].to_i)
+    redirect to("/members")
+end
+
+# UPDATE
 post '/members/:id' do
     @member = Member.new(params)
     @member.update
