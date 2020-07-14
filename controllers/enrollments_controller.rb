86bbcfa2982aba1d2c61b7f1782838dd1b728bc3
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-07-14 15:38:13 +0100

    enroll member in class success

--- controllers/enrollments_controller.rb
+++ controllers/enrollments_controller.rb
@@ -6,16 +6,16 @@ require_relative('../models/gym_class.rb')
 also_reload('../models/*')
 
 # NEW
-get '/classes/:id/enroll' do
+get '/enrollment/:id' do
     @gym_class = GymClass.find(params['id'].to_i)
     @members = Member.all()
     erb(:"enrollments/new")
 end
 
 # CREATE
-# post '/classes/:id' do
-#     @enrollment = Enrollment.new(params)
-#     @enrollment.save
-#     @gym_classes = GymClass.all()
-#     erb(:"gym_classes/index")
-# end
+post '/classes/:id/enrolled' do
+    enrollment = Enrollment.new(params)
+    enrollment.save
+    @gym_classes = GymClass.all()
+    erb(:"gym_classes/show")
+end
